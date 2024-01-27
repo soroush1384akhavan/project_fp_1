be8912943a1329b3faea276293f0708efa4fb900
soroush1384akhavan <soroush1384.akhavan@gmail.com>
2024-01-27 19:53:47 +0330

    Upgrade Done!!!

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,9 @@
-gun
+ // if (userInput_u != '0' && userInput_u != '1' && userInput_u != '2')
+            // {
+            //     while (userInput_u != '0' && userInput_u != '1' && userInput_u != '2')
+            //     {
+            //         cout << "\033[" << 14 << ";" << 45 << "H";
+            //         cout << "Please just enter the numbers in the menu: ";
+            //         userInput_u = getUserInput();
+            //     }
+            // }
